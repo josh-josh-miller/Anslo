a8765f6b2d45035d8d4a09276773ebdd10282fe5
Josh Miller <wickedhillsmedia@gmail.com>
2018-12-09 21:54:41 -0500

    defined what a section in the graph would look like

---
+++ src/interface/graphable.ts
@@ -0,0 +1,11 @@
+/**
+ * Represents what a graph looks like
+ */
+export interface Graphable {
+    references: number;
+    type: string;
+    key?: string;
+    value?: any;
+    context?: string;
+    contents?: { [key: string]: Graphable };
+}
