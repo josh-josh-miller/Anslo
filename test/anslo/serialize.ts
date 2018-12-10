c6de668540fa4759e1346592bdb3a2b5c5acaa0c
Josh Miller <wickedhillsmedia@gmail.com>
2018-12-10 00:02:43 -0500

    removed old dependencies

--- test/anslo/serialize.ts
+++ test/anslo/serialize.ts
@@ -1,6 +1,6 @@
 import Anslo = require("../../src");
 import Model from "../model";
-import { randomBytes } from "crypto";
+
 export function serialize() {
     let core = new Anslo({ User: Model.User }, "core");
     let user = new Model.User();
